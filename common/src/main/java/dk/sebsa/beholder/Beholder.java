6b0fbcbe8a49f14e651493ccbda0f55ede1ce603
Sebastian August Snoer <ssnoer@proton.me>
2023-10-10 16:51:44 +0200

    BEHOLDER

---
+++ common/src/main/java/dk/sebsa/beholder/Beholder.java
@@ -0,0 +1,66 @@
+package dk.sebsa.beholder;
+
+import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.DeferredSupplier;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import java.util.function.Supplier;
+
+public class Beholder {
+    private final String modId;
+    public final DeferredRegister<Block> BLOCKS_REGISTRY;
+    public final DeferredRegister<Item> ITEMS_REGISTRY;
+    public final DeferredRegister<CreativeModeTab> TABS_REGISTRY;
+    public final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_REGISTRY;
+
+    public Beholder(String modId) {
+        this.modId = modId;
+
+        ITEMS_REGISTRY = DeferredRegister.create(modId, Registries.ITEM);
+        BLOCKS_REGISTRY = DeferredRegister.create(modId, Registries.BLOCK);
+        TABS_REGISTRY = DeferredRegister.create(modId, Registries.CREATIVE_MODE_TAB);
+        BLOCK_ENTITY_REGISTRY = DeferredRegister.create(modId, Registries.BLOCK_ENTITY_TYPE);
+    }
+
+    public RegistrySupplier<Item> registerItem(String id, Supplier<? extends Item> item) {
+        return ITEMS_REGISTRY.register(id, item);
+    }
+
+    public RegistrySupplier<Block> registerblock(String id, Supplier<? extends Block> block) {
+        return BLOCKS_REGISTRY.register(id, block);
+    }
+
+    public DeferredSupplier<CreativeModeTab> registerTab(String id, Component title, Supplier<ItemStack> icon) {
+        return TABS_REGISTRY.register(id, () -> CreativeTabRegistry.create(title, icon));
+    }
+
+    public RegistrySupplier<Block> registerBlockWithItem(String id, Supplier<? extends Block> block, DeferredSupplier<CreativeModeTab> tab) {
+        var b = registerblock(id, block);
+        if(tab != null)
+            registerItem(id, () -> new BlockItem(b.get(), new Item.Properties().arch$tab(tab)));
+        else
+            registerItem(id, () -> new BlockItem(b.get(), new Item.Properties()));
+        return b;
+    }
+
+    public <R extends BlockEntity, T extends BlockEntityType<R>> RegistrySupplier<BlockEntityType<R>> registerBlockEntity(String id, BlockEntityType.BlockEntitySupplier<R> be, RegistrySupplier<Block> b) {
+        return BLOCK_ENTITY_REGISTRY.register(id, () -> BlockEntityType.Builder.of(be, b.get()).build(null));
+    }
+
+    public void register() {
+        BLOCKS_REGISTRY.register();
+        TABS_REGISTRY.register();
+        ITEMS_REGISTRY.register();
+        BLOCK_ENTITY_REGISTRY.register();
+    }
+}
--- common/src/main/java/dk/sebsa/yaam/YAAMRegistry.java
+++ common/src/main/java/dk/sebsa/yaam/YAAMRegistry.java
@@ -1,11 +1,10 @@
 package dk.sebsa.yaam;
 
-import dev.architectury.registry.CreativeTabRegistry;
-import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.DeferredSupplier;
 import dev.architectury.registry.registries.RegistrySupplier;
+import dk.sebsa.beholder.Beholder;
 import dk.sebsa.yaam.blocks.DryingRack;
 import dk.sebsa.yaam.blocks.entities.DryingRackEntity;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
@@ -14,29 +13,12 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class YAAMRegistry {
-    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(YetAnotherAdditionsMod.MOD_ID, Registries.CREATIVE_MODE_TAB);
-
-    public static final RegistrySupplier<CreativeModeTab> YAAM_TAB = TABS.register(
-            "yaam_tab", // Tab ID
-            () -> CreativeTabRegistry.create(
-                    Component.translatable("category.yaam_tab"), // Tab Name
-                    () -> new ItemStack(Items.LANTERN) // Icon
-            )
-    );
-
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(YetAnotherAdditionsMod.MOD_ID, Registries.BLOCK);
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(YetAnotherAdditionsMod.MOD_ID, Registries.ITEM);
-    public static final RegistrySupplier<Block> DRYING_RACK = BLOCKS.register("drying_rack", () -> new DryingRack(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)));
-
-    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(YetAnotherAdditionsMod.MOD_ID, Registries.BLOCK_ENTITY_TYPE);
-    public static final RegistrySupplier<BlockEntityType<DryingRackEntity>> DRYING_RACK_ENTITY =
-            BLOCK_ENTITIES.register("drying_rack_entity", () -> BlockEntityType.Builder.of(DryingRackEntity::new, DRYING_RACK.get()).build(null));
+    public static final Beholder BEHOLDER = new Beholder(YetAnotherAdditionsMod.MOD_ID);
+    public static final DeferredSupplier<CreativeModeTab> YAAM_TAB = BEHOLDER.registerTab("yaam_tab", Component.translatable("category.yaam_tab"), () -> new ItemStack(Items.LANTERN));
+    public static final RegistrySupplier<Block> DRYING_RACK = BEHOLDER.registerBlockWithItem("drying_rack", () -> new DryingRack(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)), YAAM_TAB);
+    public static final RegistrySupplier<BlockEntityType<DryingRackEntity>> DRYING_RACK_ENTITY = BEHOLDER.registerBlockEntity("drying_rack_entity", DryingRackEntity::new, DRYING_RACK);
 
     public static void register() {
-        BLOCKS.register();
-        TABS.register();
-        ITEMS.register("drying_rack", () -> new BlockItem(DRYING_RACK.get(), new Item.Properties().arch$tab(YAAM_TAB)));
-        ITEMS.register();
-        BLOCK_ENTITIES.register();
+        BEHOLDER.register();
     }
 }
